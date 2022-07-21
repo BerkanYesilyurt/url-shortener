5a8089210329cf004f9b3c36c74993c3e71ad27f
BerkanYesilyurt <berkan38212@gmail.com>
2022-07-21 23:08:36 +0300

    create visitors table

---
+++ database/migrations/2022_07_21_193338_create_visitors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visitors', function (Blueprint $table) {
+            $table->id();
+            $table->string('link_id');
+            $table->string('ip_address')->nullable();
+            $table->string('browser')->nullable();
+            $table->string('device')->nullable();
+            $table->text('other')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visitors');
+    }
+};
