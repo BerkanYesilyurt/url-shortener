00f0fabe8dfe3406d33829813c079a030be86c3b
BerkanYesilyurt <berkan38212@gmail.com>
2022-07-21 23:08:28 +0300

    create links table

---
+++ database/migrations/2022_07_21_193316_create_links_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id')->nullable();
+            $table->string('email')->nullable();
+            $table->string('url');
+            $table->boolean('private')->default('false');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+};
