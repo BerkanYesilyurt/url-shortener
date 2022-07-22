100f7b42f9c85b500a98901241814cc05955885f
BerkanYesilyurt <berkan38212@gmail.com>
2022-07-22 03:26:09 +0300

    web routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LinkController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::post('shorten', [LinkController::class, 'store']);
+
+Route::get('/{short_path}', [LinkController::class, 'redirect']);
