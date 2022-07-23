d281339422a9e0560f5ffb42c8b9da2b6257af6b
BerkanYesilyurt <berkan38212@gmail.com>
2022-07-23 04:04:24 +0300

    new provider

--- config/app.php
+++ config/app.php
@@ -181,7 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Jenssegers\Agent\AgentServiceProvider::class,
         /*
          * Package Service Providers...
          */
