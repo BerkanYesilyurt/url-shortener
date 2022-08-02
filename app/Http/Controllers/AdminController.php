04e2cb1bc965c428e5b1c91c4341d6fc9638b5a7
BerkanYesilyurt <berkan38212@gmail.com>
2022-08-03 00:28:05 +0300

    admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Link;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -11,14 +12,20 @@ class AdminController extends Controller
     {
         if(auth()->user()->admin_authority == 1){
             $users = $user->simplePaginate(10);
-            return view('admin', compact('users'));
+            return view('admin.index', compact('users'));
         }else{
             return redirect('/');
         }
     }
 
-    public function user($user_id){
-
+    public function user($user_id, Link $link, User $user){
+        if(auth()->user()->admin_authority == 1){
+            $userInfo = $user->where('id', '=', $user_id)->first();
+            $LinksOfUser = $link->where('user_id', '=', $user_id)->simplePaginate(10);
+            return view('admin.linksofuser', compact('LinksOfUser', 'userInfo'));
+        }else{
+            return redirect('/');
+        }
     }
 
     public function editUser($user_id){
