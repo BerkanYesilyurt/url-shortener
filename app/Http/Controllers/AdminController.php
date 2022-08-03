c57e5bfcee32007b1f913849b628d6b299a37671
BerkanYesilyurt <berkan38212@gmail.com>
2022-08-04 02:14:33 +0300

    show and edit user (admin)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Link;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class AdminController extends Controller
 {
@@ -28,7 +30,50 @@ class AdminController extends Controller
         }
     }
 
-    public function editUser($user_id){
+    public function showUser($user_id, User $user){
+        if(auth()->user()->admin_authority == 1){
+            $userInfo = $user->where('id', '=', $user_id)->first();
+            return view('admin.edituser', compact('userInfo'));
+        }else{
+            return redirect('/');
+        }
+    }
 
+    public function editUser($user_id, User $user, Request $request){
+        if(auth()->user()->admin_authority == 1){
+
+            $fields = $request->validate([
+                'name' => ['required', 'min:3'],
+                'email' => ['required', 'email'],
+                'password' => ['nullable', 'min:6'],
+                'admin_authority' => ['required', 'boolean'],
+                'API_token' => ['nullable', 'min:35', 'max:35', 'alpha_num']
+            ]);
+
+            $currentUserToken = $user->where('id', '=' , $user_id)->value('API_token');
+            $istokenExists = $user->where('API_token', '=' , $fields['API_token']);
+
+            if($istokenExists->count() == 1 && $currentUserToken == $fields['API_token']){
+                unset($fields['API_token']);
+            }elseif($istokenExists->count() == 0){
+                //passed
+            }else{
+                return back()->withErrors(['API_token' => 'The API token has already been taken.']);
+            }
+
+            if (!empty($fields['password'])){
+                $fields['password'] = bcrypt($fields['password']);
+            }else{
+                unset($fields['password']);
+            }
+
+
+            $user->where('id', '=', $user_id)->update($fields);
+
+            return back()->with('message', 'You have successfully edited user.');
+
+        }else{
+            return redirect('/');
+        }
     }
 }
