bc7d6c664d4bdfc4d138c732f1a53af53258c57c
BerkanYesilyurt <berkan38212@gmail.com>
2022-08-08 23:18:21 +0300

    API routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\LinkController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Shorten the URL
+Route::post('links', [LinkController::class, 'store'])->middleware('api');
+
+//Details of Link
+Route::get('links/{short_path}', [LinkController::class, 'getDetails'])->middleware('api');
