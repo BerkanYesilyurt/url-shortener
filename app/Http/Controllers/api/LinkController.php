6f0ee2c87f9cfe5cb1ca135cfb9cd5fd75b8d2db
BerkanYesilyurt <berkan38212@gmail.com>
2022-08-08 23:18:47 +0300

    LinkController (API)

---
+++ app/Http/Controllers/api/LinkController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Link;
+use App\Models\User;
+use App\Models\Visitor;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Jenssegers\Agent\Agent;
+use Spatie\ValidationRules\Rules\Delimited;
+
+class LinkController extends Controller
+{
+
+public function store(){
+
+}
+
+public function getDetails(){
+
+}
+
+}
